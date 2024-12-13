61f2ec5e56c8bf832f6f82cdee3efbdeaaea0e91
jorgechvz <cha22060@byui.edu>
2024-12-13 00:15:24 -0500

    refactor tailwind configuration to use ES module syntax for plugins

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
 export default {
   darkMode: ["class"],
   content: [
@@ -111,5 +112,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
